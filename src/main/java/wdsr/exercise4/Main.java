cbfcffc9de9305beb69cb7da0b41ba241291823c
nonecover <adam.jakomulski@gmail.com>
2017-05-10 23:34:14 +0200

    added logging

--- src/main/java/wdsr/exercise4/Main.java
+++ src/main/java/wdsr/exercise4/Main.java
@@ -1,21 +1,37 @@
 package wdsr.exercise4;
 
 import javax.jms.DeliveryMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import wdsr.exercise4.sender.JmsSender;
 
 public class Main {
 	static final String QUEUE_NAME = "NONECOVER.QUEUE?consumer.prefetchSize=100000";
+	final static Logger logger = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
 		JmsSender jmsSender = new JmsSender(QUEUE_NAME);
-		
-		for(int i=1;i<=10000;++i){
-			jmsSender.sendTextToQueue("test_"+String.valueOf(i), DeliveryMode.PERSISTENT);
-		}
-		for(int i=10001;i<=20000;++i){
-			jmsSender.sendTextToQueue("test_"+String.valueOf(i), DeliveryMode.NON_PERSISTENT);
-		}
-		
+
+		countTimeAndLog(() -> {
+			for (int i = 1; i <= 10000; ++i) {
+				jmsSender.sendTextToQueue("test_" + String.valueOf(i), DeliveryMode.PERSISTENT);
+			}
+		}, "10000 persistent messages sent in %d milliseconds.");
+
+		countTimeAndLog(() -> {
+			for (int i = 10001; i <= 20000; ++i) {
+				jmsSender.sendTextToQueue("test_" + String.valueOf(i), DeliveryMode.NON_PERSISTENT);
+			}
+		}, "10000 persistent messages sent in %d milliseconds.");
+
 		jmsSender.shutdown();
+	}
+
+	private static void countTimeAndLog(Runnable runnable, String message) {
+		long begTime = System.currentTimeMillis();
+		runnable.run();
+		long executeTime = System.currentTimeMillis() - begTime;
+		logger.info(String.format(message, executeTime));
 	}
 }
